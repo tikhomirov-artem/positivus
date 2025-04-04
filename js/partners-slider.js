49b50452c677ce640db59ee99bb931888323f9b1
tikhomirovofficial <arteom.tixomirow@yandex.ru>
2025-04-04 14:40:25 +0300

    welcome section: adaptive, slider adapted

--- js/partners-slider.js
+++ js/partners-slider.js
@@ -1,7 +1,28 @@
 const swiper = new Swiper(".swiper", {
     spaceBetween: 200,
     grabCursor: true,
-    autoplay: true,
+    autoplay: false,
     loop: true,
-    slidesPerView: 5
+    slidesPerView: 5,
+    breakpoints: {
+        350: {
+            slidesPerView: 2,
+            spaceBetween: 100
+        },
+        430: {
+            slidesPerView: 3,
+            spaceBetween: 60
+        },
+        630: {
+            slidesPerView: 4,
+            spaceBetween: 100
+        },
+        1024: {
+            slidesPerView: 5,
+            spaceBetween: 100
+        },
+        1024: {
+            spaceBetween: 200
+        }
+    }
 })
